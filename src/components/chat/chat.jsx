d0b037390fa7a768de5769fd89a274686a60db1d
Sara Beldi <sarabeldi01@gmail.com>
2024-07-28 17:42:31 +0100

    first part done

---
+++ src/components/chat/chat.jsx
@@ -0,0 +1,13 @@
+
+import"./chat.css"
+const Chat = () => {
+  return (
+    
+   
+    <div className='chat'>Chat</div>
+  )
+}
+
+
+
+export default Chat
---
+++ src/components/list/chatList/chatlist.jsx
@@ -0,0 +1,8 @@
+import './chatlist.css'
+const Chatlist = () => {
+  return (
+    <div className=''>Chatlist</div>
+  )
+}
+
+export default Chatlist
